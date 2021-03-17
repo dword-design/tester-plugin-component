9bc23e44f27b3311dc8108c09f457b139b6cf97f
Sebastian Landwehr <info@dword-design.de>
2021-03-17 23:04:09 +0100

    fix: run in tmp dir (#2)

--- src/index.js
+++ src/index.js
@@ -3,29 +3,33 @@ import packageName from 'depcheck-package-name'
 import { Builder, Nuxt } from 'nuxt'
 import outputFiles from 'output-files'
 import P from 'path'
+import withLocalTmpDir from 'with-local-tmp-dir'
 
 export default () => ({
-  transform: test =>
-    async function () {
-      test = { test: () => {}, ...test }
-      await outputFiles({
-        'pages/index.vue': test.page,
-        'plugins/plugin.js': endent`
-        import Self from '${test.componentPath |> replace(/\\/g, '/')}'
-        import Vue from '${packageName`vue`}'
+  transform: test => {
+    test = { test: () => {}, ...test }
+    return function () {
+      return withLocalTmpDir(async () => {
+        await outputFiles({
+          'pages/index.vue': test.page,
+          'plugins/plugin.js': endent`
+          import Self from '${test.componentPath |> replace(/\\/g, '/')}'
+          import Vue from '${packageName`vue`}'
 
-        Vue.component('Self', Self)
+          Vue.component('Self', Self)
 
-      `,
+        `,
+        })
+        const nuxt = new Nuxt({
+          createRequire: 'native',
+          dev: false,
+          plugins: [P.resolve('plugins', 'plugin.js')],
+        })
+        await new Builder(nuxt).build()
+        await nuxt.listen()
+        await test.test.call(this)
+        await nuxt.close()
       })
-      const nuxt = new Nuxt({
-        createRequire: 'native',
-        dev: false,
-        plugins: [P.resolve('plugins', 'plugin.js')],
-      })
-      await new Builder(nuxt).build()
-      await nuxt.listen()
-      await test.test.call(this)
-      await nuxt.close()
-    },
+    }
+  },
 })
