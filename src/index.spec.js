b76f09c2040eeec350bc8d0cf29d9fcdcb01061f
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-01-07 22:27:51 +0100

    fix: revert (#106)

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,4 @@
-import { endent, replace } from '@dword-design/functions'
+import { endent } from '@dword-design/functions'
 import tester from '@dword-design/tester'
 import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
 import packageName from 'depcheck-package-name'
@@ -6,11 +6,9 @@ import { execa } from 'execa'
 import outputFiles from 'output-files'
 import unifyMochaOutput from 'unify-mocha-output'
 
-const cwd = process.cwd()
-
 export default tester(
   {
-    'client mode': async () => {
+    async 'client mode'() {
       await outputFiles({
         'index.spec.js': endent`
       import { endent } from '@dword-design/functions'
@@ -64,13 +62,30 @@ export default tester(
     `,
         'package.json': JSON.stringify({ type: 'module' }),
       })
-      await execa('mocha', [
-        '--ui',
-        packageName`mocha-ui-exports-auto-describe`,
-        '--timeout',
-        80000,
-        'index.spec.js',
-      ])
+
+      const output = await execa(
+        'nyc',
+        [
+          '--reporter',
+          'lcov',
+          '--reporter',
+          'text',
+          '--cwd',
+          process.cwd(),
+          '--extension',
+          '.vue',
+          '--exclude',
+          'tmp-*',
+          'mocha',
+          '--ui',
+          packageName`mocha-ui-exports-auto-describe`,
+          '--timeout',
+          80000,
+          'index.spec.js',
+        ],
+        { all: true }
+      )
+      expect(output.all |> unifyMochaOutput).toMatchSnapshot(this)
     },
     error: async () => {
       await outputFiles({
@@ -102,13 +117,17 @@ export default tester(
         'package.json': JSON.stringify({ type: 'module' }),
       })
       await expect(
-        execa('mocha', [
-          '--ui',
-          packageName`mocha-ui-exports-auto-describe`,
-          '--timeout',
-          80000,
-          'index.spec.js',
-        ])
+        execa(
+          'mocha',
+          [
+            '--ui',
+            packageName`mocha-ui-exports-auto-describe`,
+            '--timeout',
+            80000,
+            'index.spec.js',
+          ],
+          { all: true }
+        )
       ).rejects.toThrow('Foo bar baz')
     },
     'nuxt config': async () => {
@@ -126,15 +145,16 @@ export default tester(
       export default tester({
         works: {
           files: {
-            'modules/foobar.js': "export default () => { throw new Error('foobarbaz') }",
+            'node_modules/foobar.js': "export default () => console.log('foobarbaz')",
           },
-          nuxtConfig: { build: { quiet: false }, modules: ['~/modules/foobar.js'] },
+          nuxtConfig: { build: { quiet: false }, modules: ['foobar'] },
           page: endent\`
             <template>
               <self class="foo" />
             </template>
 
           \`,
+          test: () => {},
         },
       }, [
         self({ componentPath: _require.resolve('./index.vue') }),
@@ -149,68 +169,26 @@ export default tester(
     `,
         'package.json': JSON.stringify({ type: 'module' }),
       })
-      await expect(
-        execa('mocha', [
+
+      const output = await execa(
+        'nyc',
+        [
+          '--cwd',
+          process.cwd(),
+          '--extension',
+          '.vue',
+          '--exclude',
+          'tmp-*',
+          'mocha',
           '--ui',
           packageName`mocha-ui-exports-auto-describe`,
           '--timeout',
           80000,
           'index.spec.js',
-        ])
-      ).rejects.toThrow('foobarbaz')
-    },
-    vue3: async () => {
-      await outputFiles({
-        'index.spec.js': endent`
-      import { endent } from '@dword-design/functions'
-      import tester from '${packageName`@dword-design/tester`}'
-      import self from '../src/index.js'
-      import testerPluginPuppeteer from '${packageName`@dword-design/tester-plugin-puppeteer`}'
-      import { createRequire } from 'module'
-      import { expect } from 'expect'
-
-      const _require = createRequire(import.meta.url)
-
-      export default tester({
-        works: {
-          vueVersion: 3,
-          page: endent\`
-            <template>
-              <self class="foo" />
-            </template>
-
-          \`,
-          async test() {
-            await this.page.goto('http://localhost:3000')
-            const foo = await this.page.waitForSelector('.foo')
-            expect(await foo.evaluate(el => el.innerText)).toEqual('Hello world')
-          },
-        },
-      }, [
-        self({ componentPath: _require.resolve('./index.vue') }),
-        testerPluginPuppeteer(),
-      ])
-
-    `,
-        'index.vue': endent`
-      <template>
-        <div>{{ foo }}</div>
-      </template>
-
-      <script setup>
-      const foo = ref('Hello world')
-      </script>
-
-    `,
-        'package.json': JSON.stringify({ type: 'module' }),
-      })
-      await execa('mocha', [
-        '--ui',
-        packageName`mocha-ui-exports-auto-describe`,
-        '--timeout',
-        80000,
-        'index.spec.js',
-      ])
+        ],
+        { all: true }
+      )
+      expect(output.all |> unifyMochaOutput).toMatch('foobarbaz')
     },
     async works() {
       await outputFiles({
@@ -280,15 +258,7 @@ export default tester(
         ],
         { all: true }
       )
-      expect(
-        output.all
-          |> unifyMochaOutput
-          |> replace(cwd, '')
-          |> replace(
-            '\\node_modules\\rollup\\dist\\es\\shared\\rollup.js',
-            '/node_modules/rollup/dist/es/shared/rollup.js'
-          )
-      ).toMatchSnapshot(this)
+      expect(output.all |> unifyMochaOutput).toMatchSnapshot(this)
     },
   },
   [testerPluginTmpDir()]
